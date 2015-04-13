e71da0875de1baa45976536d44f81a1f85a4465b
arendtrasmussen <arendtrasmussen@gmail.com>
2015-04-13 11:11:14 +0200

    Update ProjectManagementApp.java

--- src/projekt_02_3/ProjectManagementApp.java
+++ src/projekt_02_3/ProjectManagementApp.java
@@ -1,32 +1,29 @@
 import java.util.List;
 import java.util.LinkedList;
 
-
 public class ProjectManagementApp {
 
 	private boolean projectLeader = false;
 	private List<Employee> employees = new LinkedList<Employee>();
 	private List<Project> projects = new LinkedList<Project>();
-	
-	
-	public void addProject(Project project1) {
-		projects.add(project1);
-		}
-	
-	public void createProject(String title) throws OperationNotAllowedException  {
+	private Project project;
+
+	public void addProject(Project project) {
+		projects.add(project);
+	}
+
+	public void createProject(String title) throws OperationNotAllowedException {
 		Project project = new Project(title);
 		addProject(project);
 		if (project.getTitle().isEmpty()) {
-			throw new OperationNotAllowedException("Error: You must enter a valid name for your project",
+			throw new OperationNotAllowedException(
+					"Error: You must enter a valid name for your project",
 					"Please rename your project");
 		}
-		
+
 	}
-		
-		
-	
-	
-	public List<Project> getProjects(){
+
+	public List<Project> getProjects() {
 		return projects;
 	}
 
@@ -38,13 +35,18 @@ public class ProjectManagementApp {
 		this.projectLeader = projectLeader;
 	}
 
-	public void createActivity(String title, int budgetTime) {
-		Activity activity = new Activity(title,budgetTime);
-		addActivityToProject(activity);
-		
+	public void createActivity(String title, int budgetTime, Project project) throws OperationNotAllowedException {
+		Activity activity = new Activity(title, budgetTime);
+		project.addActivity(activity);
+		addActivityToProject(activity,project);
+		if (activity.getTitle().isEmpty() || activity.getBudgetTime()==0 || project.equals(null)) {
+			throw new OperationNotAllowedException(
+					"Error: You must enter a valid name for your activity",
+					"Please rename your activity");
+		}
 	}
 
-	private void addActivityToProject(Activity activity) {
-		
+	private void addActivityToProject(Activity activity, Project project) {
+		project.addActivity(activity);
 	}
 }
