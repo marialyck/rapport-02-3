46a882eee297d2fae99f5be456b544c256e94e42
arendtrasmussen <arendtrasmussen@gmail.com>
2015-04-13 09:45:47 +0200

    Update Activity.java

--- src/projekt_02_3/Activity.java
+++ src/projekt_02_3/Activity.java
@@ -1,5 +1,12 @@
-package projekt_02_3;
-
 public class Activity {
+	protected String title;
+	protected int budgetTime;
 
+	public Activity(String title, int budgetTime) {
+		this.title = title;
+		this.budgetTime=budgetTime;
+	}
+	public String getTitle(){
+		return title;
+	}
 }
