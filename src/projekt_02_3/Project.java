3c945ec39fcd370576eb5aec55aa763c6719afd3
arendtrasmussen <arendtrasmussen@gmail.com>
2015-04-13 09:45:21 +0200

    Update Project.java

--- src/projekt_02_3/Project.java
+++ src/projekt_02_3/Project.java
@@ -1,5 +1,26 @@
-package projekt_02_3;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Project {
+
+	protected String title;
+	protected String init;
+	public Employee employee;
+	public ProjectManagementApp proApp;
+	private Activity activity;
+	private List<Activity> activities = new LinkedList<Activity>();
+	
+	public Project(String title) {
+		this.title = title;
+	}
+
+	public void assignProjectLeader(Employee emp) {
+		emp.getInit();
+		proApp.setProjectLeader(true);
+	}
+
+	public String getTitle() {
+		return title;
+	}
 
 }
