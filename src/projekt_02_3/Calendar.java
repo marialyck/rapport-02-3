3e16668d830ffbb0b2388a835e32c9851a0ed874
Maria Lyck Carstensen <marialyckcarstensen@gmail.com>
2015-04-13 11:11:47 +0200

    Update Calendar.java

--- src/projekt_02_3/Calendar.java
+++ src/projekt_02_3/Calendar.java
@@ -4,16 +4,29 @@ import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Calendar {
-	GregorianCalendar calendar = new GregorianCalendar();
-	
-	public Calendar(Employee employee){
-		
-		
-		
-		
+
+	public int year;
+	public int month;
+	public int day;
+	public int hour;
+	public int minute;
+
+	public GregorianCalendar calendar = new GregorianCalendar();
+	static Scanner scan;
+
+	public void addEvent() {
+
 	}
 
-	
-	
+	public Calendar(int year, int month, int day, int hour,
+			int minute) {
+		this.year=year;
+		this.month=month;
+		this.day=day;
+		this.hour=hour;
+		this.minute=minute;
+		
+
+	}
 
 }
