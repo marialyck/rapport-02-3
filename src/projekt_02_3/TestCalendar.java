50018999fb4666a65102cbdbde793b6f674137cc
Daniel Hjalsted <dwhjalsted@hotmail.com>
2015-04-13 15:14:05 +0200

    Update TestCalendar.java

--- src/projekt_02_3/TestCalendar.java
+++ src/projekt_02_3/TestCalendar.java
@@ -1,55 +1,43 @@
-package projekt_02_3;
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import org.junit.Test;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
-
 public class TestCalendar {
 
-	
-	public int year;
-	public int month;
-	public int day;
-	public int hour;
-	public int minute;
-
-	static Scanner scan;
-
-	
-	public TestCalendar(int year, int month, int day, int hour,
-			int minute) {
-		this.year=year;
-		this.month=month;
-		this.day=day;
-		this.hour=hour;
-		this.minute=minute;
-	}
-	Calendar calendar = new GregorianCalendar();
-	
 	@Test
-	public void testCreateActivity() {
-		
-		calendar.set(Calendar.YEAR, 2015);
-		calendar.set(Calendar.MONTH, 10);
-		calendar.set(Calendar.DAY_OF_MONTH, 5);
-		calendar.set(Calendar.HOUR_OF_DAY, 10);
-		calendar.set(Calendar.MINUTE, 0);
-		Date startdate = calendar.getTime();
-		
-		calendar.add(Calendar.YEAR, 1);
-		calendar.add(Calendar.MONTH, 2);
-		calendar.add(Calendar.DAY_OF_MONTH, 3);
-		calendar.add(Calendar.HOUR_OF_DAY, 4);
-		calendar.add(Calendar.MINUTE, 5);
-		Date enddate = calendar.getTime();
-	}
+	public void testActivity() {
+		int year = 2015;
+		int month = 4;
+		int day = 2;
+		int hour = 15;
+		int minute = 15;
+
+		Calendar cal1 = new GregorianCalendar(year, month, day, hour, minute);
+		Date startdate = cal1.getTime();
+
+		cal1.add(Calendar.YEAR, 2);
+		cal1.add(Calendar.MONTH, 5);
+		cal1.add(Calendar.DAY_OF_MONTH, 3);
+		cal1.add(Calendar.HOUR_OF_DAY, 1);
+		cal1.add(Calendar.MINUTE, 22);
+		Date enddate = cal1.getTime();
+
+		cal1.set(Calendar.YEAR, 2017);
+		cal1.set(Calendar.MONTH, 9);
+		cal1.set(Calendar.DAY_OF_MONTH, 5);
+		cal1.set(Calendar.HOUR_OF_DAY, 16);
+		cal1.set(Calendar.MINUTE, 37);
+		Date checkdate = cal1.getTime();
+		// System.out.println(enddate);
+		// System.out.println(checkdate);
+		assertEquals(enddate, checkdate);
 
 	}
+
+}
 	
