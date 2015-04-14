38d6943a48e6d7cb6c48c60cfe575348cffbc878
Maria Lyck Carstensen <marialyckcarstensen@gmail.com>
2015-04-14 10:18:45 +0200

    Update Activity.java

--- ProjectManagement/src/Activity.java
+++ ProjectManagement/src/Activity.java
@@ -2,16 +2,16 @@ public class Activity {
 	protected String title;
 	protected int budgetTime;
 
-	public Activity(String title, int budgetTime) {
+	public Activity(String title, int budgetTime, String startdate, String enddate) {
+		
 		this.title = title;
 		this.budgetTime=budgetTime; 
 	}
+	
 	public String getTitle(){
 		return title;
 	}
-	
 	public int getBudgetTime(){
 		return budgetTime;
 	}
-
 }
