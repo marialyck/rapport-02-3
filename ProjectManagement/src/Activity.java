ac68836b3fce5420658ac4f006113ae8689f3dab
arendtrasmussen <arendtrasmussen@gmail.com>
2015-04-14 13:15:01 +0200

    TestAddEmployee 2

--- ProjectManagement/src/Activity.java
+++ ProjectManagement/src/Activity.java
@@ -1,41 +1,17 @@
-import java.util.Date;
-
-public class Activity extends Cal {
+public class Activity {
 	protected String title;
 	protected int budgetTime;
-	protected Cal cal;
 
-	public Activity(String title, int budgetTime, Cal cal) {
-		
+	public Activity(String title, int budgetTime) {
 		this.title = title;
-		this.budgetTime=budgetTime;
-		this.cal=cal;
-		
-		
+		this.budgetTime=budgetTime; 
 	}
-	
 	public String getTitle(){
 		return title;
 	}
+	
 	public int getBudgetTime(){
 		return budgetTime;
 	}
-	
-	public void setStartDate(){
-		this.startdate=startdate;
-	}
-	
-	public void setEndDate() {
-		this.enddate=enddate;
-		
-	}
-	
-	public Date getStartDate(){
-		return cal.startdate;
-		
-	}
-	public Date getEndDate(){
-		return cal.enddate;
-		
-	}
+
 }
