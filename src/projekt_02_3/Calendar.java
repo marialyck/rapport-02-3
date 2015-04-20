9063bf006c2735e3ddf5e5f948cd4f393c5983dd
Maria Lyck Carstensen <marialyckcarstensen@gmail.com>
2015-04-20 09:59:20 +0200

    Update Calendar.java

--- src/projekt_02_3/Calendar.java
+++ src/projekt_02_3/Calendar.java
@@ -5,43 +5,53 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
-public class Cal {
+public abstract class Cal {
 
-	public int year;
-	public int month;
-	public int day;
-	public int hour;
-	public int minute;
+	public int year = 1;
+	public int month = 0;
+	public int day = 0;
+	public int hour = 0;
+	public int minute = 0;
 	public Date startdate;
 	public Date enddate;
 
+	String title;
+
 	static Scanner scan;
-
-	public Cal(int year, int month, int day, int hour, int minute) {
-		this.year = year;
-		this.month = month;
-		this.day = day;
-		this.hour = hour;
-		this.minute = minute;
-	}
-
-	Calendar calendar = new GregorianCalendar();
+	protected Calendar calendar = new GregorianCalendar();
 
 	public void createActivity() {
 
-		calendar.set(Calendar.YEAR, scan.nextInt());
+		calendar.set(Calendar.YEAR, scan.nextInt()-1);
 		calendar.set(Calendar.MONTH, scan.nextInt());
 		calendar.set(Calendar.DAY_OF_MONTH, scan.nextInt());
 		calendar.set(Calendar.HOUR_OF_DAY, scan.nextInt());
 		calendar.set(Calendar.MINUTE, scan.nextInt());
-		startdate = calendar.getTime();
+		Date startdate = calendar.getTime();
 
 		calendar.add(Calendar.YEAR, scan.nextInt());
 		calendar.add(Calendar.MONTH, scan.nextInt());
 		calendar.add(Calendar.DAY_OF_MONTH, scan.nextInt());
 		calendar.add(Calendar.HOUR_OF_DAY, scan.nextInt());
 		calendar.add(Calendar.MINUTE, scan.nextInt());
-		enddate = calendar.getTime();
+		Date enddate = calendar.getTime();
+
+	}
+	public void setStartDate(Date startdate){
+		this.startdate=startdate;
+	}
+	
+	public void setEndDate(Date enddate) {
+		this.enddate=enddate;
+		
+	}
+	
+	public Date getStartDate(){
+		return startdate;
+		
+	}
+	public Date getEndDate(){
+		return enddate;
 		
 	}
 
