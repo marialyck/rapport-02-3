5319c7fb2282b7bec18345829caa37ea724fecd5
Maria Lyck Carstensen <marialyckcarstensen@gmail.com>
2015-04-20 13:16:16 +0200

    Update Activity.java

--- ProjectManagement/src/Activity.java
+++ ProjectManagement/src/Activity.java
@@ -1,17 +1,38 @@
-public class Activity {
-	protected String title;
-	protected int budgetTime;
+import java.util.Date;
 
-	public Activity(String title, int budgetTime) {
+public class Activity extends Cal {
+	protected int budgetTime;
+	protected Cal cal;
+
+	public Activity(String title, int budgetTime, Cal cal) {
+		
 		this.title = title;
-		this.budgetTime=budgetTime; 
+		this.budgetTime=budgetTime;
+		this.cal=cal;
 	}
+	
 	public String getTitle(){
 		return title;
 	}
-	
 	public int getBudgetTime(){
 		return budgetTime;
 	}
-
+	
+	public void setStartDate(Date startdate){
+		Cal.startdate=startdate;
+	}
+	
+	public void setEndDate(Date enddate) {
+		Cal.enddate=enddate;
+		
+	}
+	
+	public Date getStartDate(){
+		return Cal.startdate;
+		
+	}
+	public Date getEndDate(){
+		return Cal.enddate;
+		
+	}
 }
