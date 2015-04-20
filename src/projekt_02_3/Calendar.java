76b76b7bf5f718bf4c1e8d1582b84346c1422a13
Daniel Hjalsted <dwhjalsted@hotmail.com>
2015-04-20 09:48:57 +0200

    Update Calendar.java

--- src/projekt_02_3/Calendar.java
+++ src/projekt_02_3/Calendar.java
@@ -12,35 +12,37 @@ public class Cal {
 	public int day;
 	public int hour;
 	public int minute;
+	Date startdate;
+	Date enddate;
 
 	static Scanner scan;
 
-
-	public Cal(int year, int month, int day, int hour,
-			int minute) {
-		this.year=year;
-		this.month=month;
-		this.day=day;
-		this.hour=hour;
-		this.minute=minute;
+	public Cal(int year, int month, int day, int hour, int minute) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+		this.hour = hour;
+		this.minute = minute;
 	}
+
 	Calendar calendar = new GregorianCalendar();
+
 	public void createActivity() {
-		
+
 		calendar.set(Calendar.YEAR, scan.nextInt());
 		calendar.set(Calendar.MONTH, scan.nextInt());
 		calendar.set(Calendar.DAY_OF_MONTH, scan.nextInt());
 		calendar.set(Calendar.HOUR_OF_DAY, scan.nextInt());
 		calendar.set(Calendar.MINUTE, scan.nextInt());
-		Date startdate = calendar.getTime();
-		
+		startdate = calendar.getTime();
+
 		calendar.add(Calendar.YEAR, scan.nextInt());
 		calendar.add(Calendar.MONTH, scan.nextInt());
 		calendar.add(Calendar.DAY_OF_MONTH, scan.nextInt());
 		calendar.add(Calendar.HOUR_OF_DAY, scan.nextInt());
 		calendar.add(Calendar.MINUTE, scan.nextInt());
-		Date enddate = calendar.getTime();
+		enddate = calendar.getTime();
+		
 	}
 
-	}
-	
+}
