50a63ceb1da9c8bea53fa4363e9e43548b39d4f7
Maria Lyck Carstensen <marialyckcarstensen@gmail.com>
2015-04-20 13:15:45 +0200

    Update Cal.java

--- ProjectManagement/src/Cal.java
+++ ProjectManagement/src/Cal.java
@@ -1,43 +1,67 @@
+package project023;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.List;
 import java.util.Scanner;
 
-import projekt_02_3.Activity;
-
-public abstract class Cal{
+public abstract class Cal {
 
 	public int year = 1;
 	public int month = 0;
 	public int day = 0;
 	public int hour = 0;
 	public int minute = 0;
-	public Date startdate;
-	public Date enddate;
-	
+	public static Date startdate;
+	public static Date enddate;
+
 	String title;
 
 	static Scanner scan;
-	protected Calendar calendar = new GregorianCalendar();
-	public void createActivity() {
-		
-		calendar.set(Calendar.YEAR, scan.nextInt()-1);
-		calendar.set(Calendar.MONTH, scan.nextInt());
-		calendar.set(Calendar.DAY_OF_MONTH, scan.nextInt());
-		calendar.set(Calendar.HOUR_OF_DAY, scan.nextInt());
-		calendar.set(Calendar.MINUTE, scan.nextInt());
-		Date startdate = calendar.getTime();
-		
-		calendar.add(Calendar.YEAR, scan.nextInt());
-		calendar.add(Calendar.MONTH, scan.nextInt());
-		calendar.add(Calendar.DAY_OF_MONTH, scan.nextInt());
-		calendar.add(Calendar.HOUR_OF_DAY, scan.nextInt());
-		calendar.add(Calendar.MINUTE, scan.nextInt());
-		Date enddate = calendar.getTime();
+	protected static Calendar calendar = new GregorianCalendar();
 	
-		
+	public static void openScanner(){
+			scan = new Scanner(System.in);
 	}
+	
+
+	public static void createActivity(Date startdate, Date enddate, String title) {	
+		
+		System.out.println("Hi SweetiePie, Please enter a name for the activity");
+		title = scan.next();
+		
+		System.out.println("Please enter a start year.....");
+		calendar.set(Calendar.YEAR, scan.nextInt() - 1);
+		
+		System.out.println("Please enter a week");
+		calendar.set(Calendar.WEEK_OF_YEAR, scan.nextInt());
+		startdate = calendar.getTime();
+		
+		
+		System.out.println("So..emmm... how many weeks <3");
+		calendar.set(Calendar.WEEK_OF_YEAR, scan.nextInt());
+		enddate = calendar.getTime();
+		
+
 
 	}
-	
+
+	public void setStartDate(Date startdate) {
+		Cal.startdate = startdate;
+	}
+
+	public void setEndDate(Date enddate) {
+		Cal.enddate = enddate;
+
+	}
+
+	public Date getStartDate() {
+		return startdate;
+
+	}
+
+	public Date getEndDate() {
+		return enddate;
+
+	}
+}
