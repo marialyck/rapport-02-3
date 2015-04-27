5b77ecd4a478a5ea07fa08f719602b77691fe220
arendtrasmussen <arendtrasmussen@gmail.com>
2015-04-27 11:49:42 +0200

    Update TestAddEmployee.java

--- ProjectManagement/src/TestAddEmployee.java
+++ ProjectManagement/src/TestAddEmployee.java
@@ -5,16 +5,17 @@ import org.junit.Test;
 public class TestAddEmployee {
 	@Test
 	public void NotProjectLeader() throws Exception {
-		Employee emp1 = new Employee("CHAR");
-		Employee emp2 = new Employee("BARS");
+		ProjectManagementApp proApp = new ProjectManagementApp();
+		Employee emp1 = new Employee("CHAR", proApp);
+		Employee emp2 = new Employee("BARS", proApp);
 		Project pro1 = new Project("SwagForLife");
 		Cal cal = new Cal();
 		Activity act1 = new Activity("yolo1", 420, cal);
 		pro1.setProjectLeader(emp1);
 		// step 1) an employee tries to add a crew to a project but the employee
 		// is not a project leader
-		ProjectManagementApp proApp = new ProjectManagementApp();
-		proApp.createProject("yolo");
+		
+		proApp.createProject("yolo ");
 		assertFalse(pro1.isProjectLeader(emp2));
 		try {
 			emp2.addEmployeeToActivity(pro1, act1, emp1, cal);
@@ -35,15 +36,16 @@ public class TestAddEmployee {
 
 	@Test
 	public void testNonExistentEmployee() throws OperationNotAllowedException {
-		Employee emp1 = new Employee("CHAR");
-		Employee emp2 = new Employee("FAIL");
+		ProjectManagementApp proApp = new ProjectManagementApp();
+		Employee emp1 = new Employee("CHAR", proApp);
+		Employee emp2 = new Employee("FAIL", proApp);
 		Project pro1 = new Project("SwagForLife");
 		Cal cal = new Cal();
 		Activity act1 = new Activity("yolo1", 420, cal);
 		pro1.setProjectLeader(emp1);
 		// step 1) an employee tries to add a crew to a project but the employee
 		// is not a project leader
-		ProjectManagementApp proApp = new ProjectManagementApp();
+		
 		proApp.createProject("yolo");
 		try {
 			emp1.addEmployeeToActivity(pro1, act1, emp2, cal);
