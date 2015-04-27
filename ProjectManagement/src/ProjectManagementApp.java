846235227322332d9e30a37ea485cea1460ed464
arendtrasmussen <arendtrasmussen@gmail.com>
2015-04-27 11:48:45 +0200

    Update ProjectManagementApp.java

--- ProjectManagement/src/ProjectManagementApp.java
+++ ProjectManagement/src/ProjectManagementApp.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 public class ProjectManagementApp {
 
 	private List<Project> projects = new LinkedList<Project>();
-	
+	protected List<Employee> employees = new LinkedList<Employee>();
 
 	public void addProject(Project project) {
 		projects.add(project);
@@ -12,24 +12,33 @@ public class ProjectManagementApp {
 
 	public void createProject(String title) throws OperationNotAllowedException {
 		Project project = new Project(title);
-		
+
 		if (project.getTitle().isEmpty()) {
 			throw new OperationNotAllowedException(
 					"Error: You must enter a valid name for your project",
 					"Please rename your project");
 		}
-//		
-//		project.setProjectLeader(projectLeader);
+		//
+		// project.setProjectLeader(projectLeader);
 		addProject(project);
-		
 
+	}
+
+	public boolean search(String init) {
+		for (Employee employee : employees) {
+			if (employee.getInit().contains(init)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void addEmployee(Employee employee) {
+		employees.add(employee);
 	}
 
 	public List<Project> getProjects() {
 		return projects;
 	}
 
-	
-
-	
 }
