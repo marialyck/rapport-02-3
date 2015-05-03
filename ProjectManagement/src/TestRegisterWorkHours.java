b7427557e595177ea18b717a8c017c6790f82a38
arendtrasmussen <arendtrasmussen@gmail.com>
2015-05-03 10:59:35 +0200

    Update TestRegisterWorkHours.java

--- ProjectManagement/src/TestRegisterWorkHours.java
+++ ProjectManagement/src/TestRegisterWorkHours.java
@@ -4,9 +4,31 @@ import org.junit.Test;
 
 public class TestRegisterWorkHours {
 	@Test
-	public void testNegativeTime(){
-		Employee emp = new Employee("CHAR");
-		
+	public void testRegisterWorkHours() throws Exception {
+		ProjectManagementApp proApp = new ProjectManagementApp();
+		Employee emp = new Employee("CHAR", proApp);
+		Cal cal = new Cal();
+		Activity act1 = new Activity("Smokémon",1000,cal);
+		emp.registerWorkHours(act1, 420);
+		assertEquals(emp.getWorkHours(act1), 420);
 	}
 
+	@Test
+	public void testNegativeHours()throws Exception{
+		ProjectManagementApp proApp = new ProjectManagementApp();
+		Employee emp = new Employee("CHAR", proApp);
+		Cal cal = new Cal();
+		Activity act1 = new Activity("Smokémon",1000,cal);
+
+		try {
+			emp.registerWorkHours(act1, -420);
+			fail("OperaionNotAllowedException exception should have been thrown");
+		} catch (OperationNotAllowedException e) {
+			// Step 2) Throw error message
+			// Check that the exception thrown has the correct error message and knows which operation failed.
+			
+			assertEquals("Cannot register a negative amount of hours", e.getMessage());
+			assertEquals("Please try to register your work hours again",e.getOperation());
+		}
+	}
 }
