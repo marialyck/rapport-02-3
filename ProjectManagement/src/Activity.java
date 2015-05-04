a91dd7102dd32455690b0ea1cfb7578bb734c1de
Maria Lyck Carstensen <marialyckcarstensen@gmail.com>
2015-05-04 08:46:16 +0200

    Update Activity.java

--- ProjectManagement/src/Activity.java
+++ ProjectManagement/src/Activity.java
@@ -1,17 +1,19 @@
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.GregorianCalendar;
 import java.util.Date;
 
-public class Activity extends Cal {
+public class Activity {
 	protected int budgetTime;
-	protected Cal cal;
+	Cal cal;
 	protected List<Employee> employees = new LinkedList<Employee>();
+	private String title;
 
-	public Activity(String title, int budgetTime, Cal cal) {
-		
+	public Activity(String title, int budgetTime) {
 		this.title = title;
 		this.budgetTime = budgetTime;
-		this.cal = cal;	
+
 	}
 
 	public String getTitle() {
