63f02cc545fd4d41e42c5e3d085bb26c23b54c44
Daniel Hjalsted <dwhjalsted@hotmail.com>
2015-05-10 11:02:14 +0200

    Update Activity.java

--- ProjectManagement/src/Activity.java
+++ ProjectManagement/src/Activity.java
@@ -7,25 +7,19 @@ import java.util.GregorianCalendar;
 import java.util.Date;
 
 public class Activity {
-	protected int budgetTimeHours;
 	protected static int budgetTimeWeeks;
 	Cal cal;
 	protected List<Employee> employees = new LinkedList<Employee>();
 	private String title;
 
-	public Activity(String title, int budgetTimeHours, int budgetTimeWeeks) {
+	public Activity(String title, int budgetTimeWeeks) {
 		this.title = title;
-		this.budgetTimeHours = budgetTimeHours;
 		this.budgetTimeWeeks = budgetTimeWeeks;
 
 	}
 
 	public String getTitle() {
 		return title;
-	}
-
-	public int getBudgetTimeHours() {
-		return budgetTimeHours;
 	}
 	
 	public int getBudgetTimeWeeks() {
