434b03fa5997b3379278ef5a42707284e3f6458e
arendtrasmussen <arendtrasmussen@gmail.com>
2015-05-10 10:22:56 +0200

    Update Cal.java

--- ProjectManagement/src/Cal.java
+++ ProjectManagement/src/Cal.java
@@ -21,64 +21,26 @@ public class Cal {
 //		scan = new Scanner(System.in);
 //	}
 	
-	public Cal(int startyear, int startweek, int endyear, int endweek){
+	public Cal(){
 		this.startyear = calendar.get(Calendar.YEAR);
 		this.startweek = calendar.get(Calendar.WEEK_OF_YEAR);
 		this.endyear = calendar.get(Calendar.YEAR);
 		this.endweek = calendar.get(Calendar.WEEK_OF_YEAR);
 	}
-	
-	
 
-	public int getStartyear() {
-		return startyear;
-	}
-
-	public void setStartyear(int startyear) {
-		this.startyear = startyear;
-	}
-
-	public int getStartweek() {
-		return startweek;
-	}
-
-	public void setStartweek(int startweek) {
-		this.startweek = startweek;
-	}
-	public int getEndweek(){
-		int count = 0;
+	public void setDate(int year, int week){
+		calendar.set(GregorianCalendar.YEAR,year);
+		calendar.set(GregorianCalendar.WEEK_OF_YEAR,week);
 		
-		if (startweek + Activity.budgetTimeWeeks < 52){
-			endweek = startweek + Activity.budgetTimeWeeks;
-		}
-		else if(startweek + Activity.budgetTimeWeeks > 52){
-			for(int i = startweek + Activity.budgetTimeWeeks; i >=52; i = i - 52) {
-				count = count + 1;
-				endweek = i;
-				
-				}
-			}
-		endyear = startyear + count;
-		return endweek;
 	}
-	
-	public int getEndyear(){
-		return endyear;
+	public void getDate(){
+		calendar.get(Calendar.YEAR);
+		calendar.get(Calendar.WEEK_OF_YEAR);
 	}
-	
-//	public int getEndyear() {
-//		return endyear;
-//	}
-//
-	public void setEndyear(int endyear) {
-		this.endyear = endyear;
+	public int getYear(){
+		return calendar.get(Calendar.YEAR);
 	}
-//
-//	public int getEndweek() {
-//		return endweek;
-//	}
-//
-	public void setEndweek(int endweek) {
-		this.endweek = endweek;
+	public int getWeekOfYear(){
+		return calendar.get(Calendar.WEEK_OF_YEAR);
 	}
 }
