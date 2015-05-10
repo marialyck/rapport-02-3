ec738d5c6fec23e847d6f651696ac8d458556157
arendtrasmussen <arendtrasmussen@gmail.com>
2015-05-10 11:03:11 +0200

    Update Activity.java

--- ProjectManagement/src/Activity.java
+++ ProjectManagement/src/Activity.java
@@ -1,30 +1,31 @@
-package project023;
-
-import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.GregorianCalendar;
 import java.util.Date;
 
-public class Activity {
-	protected static int budgetTimeWeeks;
-	Cal cal;
+public class Activity extends Cal {
+	protected int budgetTime;
+	protected Cal cal;
 	protected List<Employee> employees = new LinkedList<Employee>();
-	private String title;
+	public List<Integer> workHours = new LinkedList<Integer>();
+	Cal calStart = new Cal();
+	Cal calEnd = new Cal();
+	
 
-	public Activity(String title, int budgetTimeWeeks) {
+	public Activity(String title, int budgetTime, int startYear, int startWeek, int endYear, int endWeek) { 
 		this.title = title;
-		this.budgetTimeWeeks = budgetTimeWeeks;
-
+		this.budgetTime = budgetTime;
+		calEnd.setDate(startYear, startWeek);
+		calStart.setDate(endYear, endWeek);
 	}
 
 	public String getTitle() {
 		return title;
 	}
-	
-	public int getBudgetTimeWeeks() {
-		return budgetTimeWeeks;
+
+	public int getBudgetTime() {
+		return budgetTime;
 	}
+
 	public void addEmployee(Employee employee) {
 		employees.add(employee);
 	}
@@ -32,4 +33,5 @@ public class Activity {
 	public List<Employee> getEmployees() {
 		return employees;
 	}
+
 }
