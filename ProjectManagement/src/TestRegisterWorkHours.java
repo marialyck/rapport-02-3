81408ef1763df46ef555c66061d7c5dbc436831e
Daniel Hjalsted <dwhjalsted@hotmail.com>
2015-05-11 14:59:58 +0200

    Update TestRegisterWorkHours.java

--- ProjectManagement/src/TestRegisterWorkHours.java
+++ ProjectManagement/src/TestRegisterWorkHours.java
@@ -6,13 +6,13 @@ public class TestRegisterWorkHours {
 	@Test
 	public void testRegisterWorkHours() throws Exception {
 		ProjectManagementApp proApp = new ProjectManagementApp();
-		Employee emp = new Employee("CHAR", proApp);
+		Employee emp = new Employee("PrinceCharming", proApp);
 		proApp.addEmployee(emp);
-		Project pro1 = new Project("SwagForLife",2015, 1, 2017, 1);
+		Project pro1 = new Project("MakeWeapon",2015, 1, 2017, 1);
 		proApp.addProject(pro1);
 		pro1.setProjectLeader(emp);
 		Cal cal = new Cal();
-		Activity act1 = new Activity("Smokémon",1000,2015,2,2016,2);
+		Activity act1 = new Activity("SwingAxe",1000,2015,2,2016,2);
 		emp.addEmployeeToActivity(pro1, act1, emp);
 		emp.registerWorkHours(act1, 420);
 		assertEquals(emp.getWorkHours(act1), 420);
@@ -23,7 +23,7 @@ public class TestRegisterWorkHours {
 		ProjectManagementApp proApp = new ProjectManagementApp();
 		Employee emp = new Employee("CHAR", proApp);
 		Cal cal = new Cal();
-		Activity act1 = new Activity("Smokémon",1000,2015,2,2016,2);
+		Activity act1 = new Activity("CatchThemAll",1000,2015,2,2016,2);
 
 		try {
 			emp.registerWorkHours(act1, -420);
