e5a2a9c256cc028c250a88cb7ada11715bbaf341
arendtrasmussen <arendtrasmussen@gmail.com>
2015-05-11 15:03:56 +0200

    Update Cal.java

--- ProjectManagement/src/Cal.java
+++ ProjectManagement/src/Cal.java
@@ -1,25 +1,20 @@
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-//import java.util.Scanner;
+
 
 public class Cal {
 	public int startyear = Calendar.YEAR;
 	public int startweek = Calendar.WEEK_OF_YEAR;
 	public int endyear = Calendar.YEAR;
 	public int endweek = Calendar.WEEK_OF_YEAR;
-
+ 
 	public Date startdate;
 	public Date enddate;
 
 	String title;
 
-//	Scanner scan;
 	protected Calendar calendar = new GregorianCalendar();
-
-//	public void openScanner() {
-//		scan = new Scanner(System.in);
-//	}
 	
 	public Cal(){
 		this.startyear = calendar.get(Calendar.YEAR);
@@ -33,10 +28,7 @@ public class Cal {
 		calendar.set(GregorianCalendar.WEEK_OF_YEAR,week);
 		
 	}
-	public void getDate(){
-		calendar.get(Calendar.YEAR);
-		calendar.get(Calendar.WEEK_OF_YEAR);
-	}
+	
 	public int getYear(){
 		return calendar.get(Calendar.YEAR);
 	}
