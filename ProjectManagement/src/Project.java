59f21cd3645ba59c0cb1c59a6cc16f071744e0fa
arendtrasmussen <arendtrasmussen@gmail.com>
2015-05-11 14:52:38 +0200

    Update Project.java

--- ProjectManagement/src/Project.java
+++ ProjectManagement/src/Project.java
@@ -8,50 +8,44 @@ public class Project {
 	public ProjectManagementApp proApp;
 	private Employee projectLeader;
 	private List<Activity> activities = new LinkedList<Activity>();
-	private Activity activity;
-	
+
 	Cal calStart = new Cal();
 	Cal calEnd = new Cal();
-	public Project(String title, int startYear, int startWeek, int endYear, int endWeek) {
+
+	public Project(String title, int startYear, int startWeek, int endYear,
+			int endWeek) {
 		this.title = title;
 		calEnd.setDate(startYear, startWeek);
 		calStart.setDate(endYear, endWeek);
 	}
 
-	
-
 	public String getTitle() {
 		return title;
 	}
 
-
-
 	public void setProjectLeader(Employee projectLeader) {
-		// TODO Auto-generated method stub
-		this.projectLeader = projectLeader; 
+
+		this.projectLeader = projectLeader;
 	}
-	
+
 	public void addActivity(Activity activity) {
-		// TODO Auto-generated method stub
+
 		activities.add(activity);
 	}
 
-
-
 	public List<Activity> getActivities() {
-		// TODO Auto-generated method stub
+
 		return activities;
 	}
 
 	public boolean isProjectLeader(Employee employee) {
-		// TODO Auto-generated method stub
+
 		return projectLeader.equals(employee);
 	}
 
 	public Employee getProjectLeader() {
-		// TODO Auto-generated method stub
+
 		return projectLeader;
 	}
-
 
 }
