2109ac5c313b55c183c04e272975c9fb58de4a68
arendtrasmussen <arendtrasmussen@gmail.com>
2015-05-11 12:27:15 +0200

    Create TestAbsence.java

---
+++ ProjectManagement/src/TestAbsence.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestAbsence {
+@Test
+public void testRegisterAbsence(){
+	ProjectManagementApp proApp = new ProjectManagementApp();
+	Absence absence = new Absence(10, 2015, 1, 2016, 1);
+	Employee emp = new Employee("stupid-ass chinaman", proApp);
+	emp.registerAbsence(emp, absence);
+	assertEquals(absence.getEmployees().size(),1);
+}
+@Test
+public void testEraseAbsence(){
+	ProjectManagementApp proApp = new ProjectManagementApp();
+	Absence absence = new Absence(50, 2015, 2, 2016, 2);
+	Employee emp1 = new Employee("char",proApp);
+	Employee emp2 = new Employee("lass",proApp);
+	emp1.registerAbsence(emp1, absence);
+	emp2.registerAbsence(emp2, absence);
+	absence.eraseAbsence(absence.getEmployees(), "char");
+	assertEquals(1,absence.getEmployees().size());
+}
+}
