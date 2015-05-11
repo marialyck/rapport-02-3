fa2720f1ea5503e1ca7f65b2ee5ec61e5b638f90
Maria Lyck Carstensen <marialyckcarstensen@gmail.com>
2015-05-11 16:33:12 +0200

    Update Employee.java

--- ProjectManagement/src/Employee.java
+++ ProjectManagement/src/Employee.java
@@ -1,9 +1,11 @@
+package project023;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
 
+	private boolean projectLeader;
 	private String init;
 	private List<Activity> activities = new LinkedList<Activity>();
 	private ProjectManagementApp proApp;
@@ -20,7 +22,7 @@ public class Employee {
 
 	public void createActivity(String title, int budgetTime, Project project,
 			int startYear, int startWeek, int endYear, int endWeek) throws OperationNotAllowedException {
-		if (!project.isProjectLeader(this)) {
+		if (!this.isProjectLeader()) {
 			throw new OperationNotAllowedException(
 					"You cannot create an activity if you're not a project leader",
 					"please request assistance from a project leader");
@@ -30,27 +32,32 @@ public class Employee {
 					"You cannot create a project with a date that has been surpassed",
 					"please redo the end date");
 		}
-		if(startYear == endYear){
-		if (startWeek > endWeek) {
-			throw new OperationNotAllowedException(
-					"You cannot create a project with a date that has been surpassed",
-					"please redo the end date");
-		}}
-		Activity activity = new Activity(title, budgetTime, startYear, startWeek, endYear, endWeek);
-		if (activity.getTitle().isEmpty() || activity.getBudgetTime() == 0) {
+		if (startYear == endYear){
+			if (startWeek > endWeek) {
+				throw new OperationNotAllowedException(
+						"You cannot create a project with a date that has been surpassed",
+						"please redo the end date");
+			}
+		}
+		if (title.isEmpty() || budgetTime == 0) {
 			throw new OperationNotAllowedException(
 					"Error: You must enter a valid name for your activity and a valid time for the activity",
 					"Please redo your activity");
 		}
-
+		
+		Activity activity = new Activity(title, budgetTime, startYear, startWeek, endYear, endWeek);
 		project.addActivity(activity);
 
+	}
+
+	private boolean isProjectLeader() {
+		return projectLeader;
 	}
 
 	public void addEmployeeToActivity(Project project, Activity activity,
 			Employee employee) throws OperationNotAllowedException {
 	
-		if (!project.isProjectLeader(this)) {
+		if (!this.isProjectLeader()) {
 
 			throw new OperationNotAllowedException(
 					"You cannot add an employee to a project unless you're a project leader",
@@ -84,7 +91,7 @@ public class Employee {
 
 	public void followUp(Employee employee, Project project, Activity activity)
 			throws OperationNotAllowedException {
-		if (!project.isProjectLeader(this)) {
+		if (!this.isProjectLeader()) {
 
 			throw new OperationNotAllowedException(
 					"you cannot follow-up on employees unless you're a project leader",
@@ -100,7 +107,7 @@ public class Employee {
 
 	public void getReport(Project project, Activity activity)
 			throws OperationNotAllowedException {
-		if (!project.isProjectLeader(this)) {
+		if (!this.isProjectLeader()) {
 
 			throw new OperationNotAllowedException(
 					"you cannot get a report unless you're a project leader",
@@ -121,5 +128,10 @@ public class Employee {
 			}
 		}
  
+	}
+
+	public void setProjectLeader(boolean projectLeader) {
+		this.projectLeader = projectLeader;
+		
 	}
 }
